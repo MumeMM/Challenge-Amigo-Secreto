049e21af0804d18d77e3f18ba91192b14fc25e4e
MumeMM <eduardomumo3@gmail.com>
2025-03-18 13:30:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+let recuadro = document.getElementById('amigo');
+let lista = document.getElementById('listaAmigos');
+
+function agregarAmigo() {
+
+    let amigo = recuadro.value;
+    let regex = /^[A-Za-zÁ-Úá-úñÑ ]+$/;
+
+    if (amigo === "" || !regex.test(amigo)) {
+        alert("Debe ingresar un nombre válido (solo letras y espacios).");
+        estableceBlanco();
+        return;
+    }
+
+    amigos.push(amigo);
+    estableceBlanco();
+    console.log(amigos);
+    mostrarAmigos();
+
+}
+
+function estableceBlanco() {
+
+    recuadro.value = "";
+}
+
+function mostrarAmigos() {
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+
+}
+
+
+function sortearAmigo() {
+
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    lista.innerHTML = "<li>" + `El amigo sorteado es ${amigoSorteado}` + "</li>";
+    lista.value = "";
+    amigos = [];
+}
